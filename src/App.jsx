9e430c28455951d4215d5c385c4145c68bc96b55
Marko Vidaković <markovidakovic29@gmail.com>
2023-05-24 13:29:14 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return <h1>gench</h1>;
+  return <Outlet />;
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <button className="bg-neutral-950 px-4 py-2.5 rounded-md text-neutral-50 font-bold text-sm">
+      Button signup
+    </button>
+  );
+}
+
+export default Button;
